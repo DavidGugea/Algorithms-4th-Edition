4451a5ed305c6c0ab0dbe3b9cee41d611ee23977
David Gugea <davidgugea@yahoo.com>
2023-03-14 21:45:41 +0100

    Chapter 4

--- Algo/src/main/java/Chapter4/Chapter44/Research/IndexedMinHeap.java
+++ Algo/src/main/java/Chapter4/Chapter44/Research/IndexedMinHeap.java
@@ -1,41 +1,103 @@
 package Chapter4.Chapter44.Research;
 
+import java.util.HashMap;
+
 public class IndexedMinHeap {
     private int[] heap;
-    private int[] index;  // maps object indices to heap indices
-    private int[] reverseIndex; // maps heap indices to object indices
-
-    private int size;
     private int capacity;
+    private int currentIndex = 0;
+
+    // positionMap[value] = index of value in heap
+    private HashMap<Integer, Integer> positionMap = new HashMap<>();
 
     public IndexedMinHeap(int capacity) {
         this.capacity = capacity;
-        size = 0;
-        heap = new int[capacity + 1];
-        index = new int[capacity + 1];
-        reverseIndex = new int[capacity + 1];
+        this.heap = new int[capacity];
+    }
 
-        for (int i = 0 ; i <= capacity; i++) {
-            reverseIndex[i] = -1;
+    public boolean contains(int value) {
+        return this.positionMap.containsKey(value);
+    }
+
+    public int parent(int i) {
+        return (i - 1) / 2;
+    }
+
+    public int left(int i) {
+        return i * 2 + 1;
+    }
+
+    public int right(int i) {
+        return i * 2 + 2;
+    }
+
+    public void swap(int i, int j) {
+        // i, j -> indexes
+        int valueI = heap[i];
+        int valueJ = heap[j];
+
+        // Swap indexes in the position map
+        positionMap.replace(valueI, j);
+        positionMap.replace(valueJ, i);
+
+        // Swap values in the heap
+        int temp = heap[i];
+        heap[i] = heap[j];
+        heap[j] = temp;
+    }
+
+    public void swim(int i) {
+        while (i != 0 && heap[i] < parent(i)) {
+            swap(i, parent(i));
+            i = parent(i);
         }
     }
 
-    public boolean isEmpty() {
-        return size == 0;
+    public void sink(int i) {
+        if (i * 2 + 1 >= (this.capacity - 1)) {
+            // If there are no kids, return
+            return;
+        }
+
+        // Get the smallest child of heap[i];
+        int smallest = i*2+1;
+
+        if (heap[i*2+1] < heap[i*2+2]) {
+            smallest += 1;
+        }
+
+        // Swap heap values with the smallest child
+        swap(i, smallest);
+
+        // Sink at the smallest index
+        sink(smallest);
     }
 
-    public boolean contains(int i) {
-        return reverseIndex[i] != -1;
+    public void insert(int value) {
+        this.heap[currentIndex] = value;
+        this.positionMap.put(value, currentIndex);
+
+        currentIndex++;
+        swim(currentIndex);
     }
 
-    public int size() {
-        return size;
+    public int extractMin() {
+        // Swap root with the last element and delete the root
+        int min = this.heap[0];
+        this.positionMap.remove(min);
+
+        this.heap[0] = this.heap[currentIndex];
+        this.heap[currentIndex] = -1;
+        currentIndex--;
+
+        // Sink the new root
+        sink(0);
+
+        return min;
     }
 
-    public void insert(int i, int key) {
-        if (size >= capacity)
-            throw new IllegalStateException("Heap size exceeded.");
-
-        
+    public void decreaseKey(int value, int newValue) {
+        int index = positionMap.get(value);
+        swim(index);
     }
 }
